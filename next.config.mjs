d68915e8a99e75700239860a8d436c47f1f2705c
sawthetphyoe <sawthetphyoe28498@gmail.com>
2024-02-16 01:07:21 +0630

    update next config cspHeader

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,5 @@
 const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
+    upgrade-insecure-requests
 `
 
 /** @type {import('next').NextConfig} */
