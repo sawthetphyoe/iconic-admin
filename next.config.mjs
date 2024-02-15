3496b79f1a93b0ee112704eb9784e8363298df88
sawthetphyoe <sawthetphyoe28498@gmail.com>
2024-02-16 01:03:46 +0630

    update next config for content-security-policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,17 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
@@ -6,7 +20,20 @@ const nextConfig = {
     env: {
         API_URL: process.env.API_URL,
         STORAGE_URL: process.env.STORAGE_URL,
-    }
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: cspHeader.replace(/\n/g, ''),
+                    },
+                ],
+            },
+        ]
+    },
 };
 
 export default nextConfig;
